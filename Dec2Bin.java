a217ebf71d292ffd69bd20caad6ce44a4ad80101
Shiyu <69575980+Shiyu-Wang-gif@users.noreply.github.com>
2020-08-18 16:13:17 +0800

    Add files via upload

---
+++ Dec2Bin.java
@@ -0,0 +1,34 @@
+/*
+Author: Shiyu Wang
+Funtion: Convert a decimal number to a binary number
+Created at 2020/8/18
+*/
+import java.util.Scanner;
+
+public class Dec2Bin {
+	public static void main(String[] args) {
+		// declare variables
+		Scanner inputScanner = new Scanner(System.in);
+		int inputNumber, decNumber, quotient, remainder;
+		String binNumber = "";
+		
+		// scan input number from cmd
+		System.out.print("Enter the number to be converted: ");
+		inputNumber = inputScanner.nextInt();
+		decNumber = inputNumber;
+		
+		// convert a decimal number to a binary number
+		do {
+			quotient = decNumber / 2;
+			remainder = decNumber % 2;
+			binNumber = binNumber + String.valueOf(remainder);
+			decNumber = quotient;
+		} while (quotient > 0);
+		StringBuilder sb = new StringBuilder(binNumber);
+		sb.reverse();
+		binNumber = sb.toString();
+		
+		// output results
+		System.out.println(decNumber + " ---> " + binNumber);
+	}
+}
